c65d2c10fcc606b0411837f06e8363ad060ac515
Cuda Chen <clh960524@gmail.com>
2018-08-23 17:49:10 +0800

    [change] let input data can be read into 2D array

--- pnn.cr
+++ pnn.cr
@@ -2,26 +2,36 @@
 #    puts "pnn"
 #end
 class Pnn
-  def initialize
+  property train_data
+  property train_result
+  property test_data
+
+  def initialize()
+    @train_data = [] of Array(Float64)
+    @train_result = [] of Array(Float64)
+    @test_data = [] of Array(Float64)
   end
 
   def read_files()
     # read train_data.txt
     puts "reading train_data.txt ..."
-    File.open("./data/train_data.txt", "r").each_line do |temp|
-      puts temp.chomp
+    File.each_line("./data/train_data.txt") do |line|
+      train_data << line.chomp.split.map &.to_f
     end
+    pp train_data
 
     # read train_result.txt
     puts "reading train_result.txt ..."
-    File.open("./data/train_result.txt", "r").each_line do |temp|
-      puts temp.chomp
+    File.each_line("./data/train_result.txt") do |line|
+      train_result << line.chomp.split.map &.to_f
     end
+    pp train_result
 
     # read test_data.txt
     puts "reading test_data.txt ..."
-    File.open("./data/test_data.txt", "r").each_line do |temp|
-      puts temp.chomp
+    File.each_line("./data/test_data.txt") do |line|
+      test_data << line.chomp.split.map &.to_f
     end
+    pp test_data
   end
 end
